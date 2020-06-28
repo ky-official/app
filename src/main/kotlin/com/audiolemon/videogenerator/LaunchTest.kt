7b4225314d4e34dd3cb4e40f3784d0140a12cbbc
ky-official <65351617+ky-official@users.noreply.github.com>
2020-06-28 16:58:59 +0100

    Fixed dependency issues in the maven. Fixed code compatibility for jdk 1.8.

--- src/main/kotlin/com/audiolemon/videogenerator/LaunchTest.kt
+++ src/main/kotlin/com/audiolemon/videogenerator/LaunchTest.kt
@@ -5,7 +5,7 @@ class LaunchTest(){
 }
 fun main(args: Array<String>){
 
-   // LemonDBManager.close()
+    //LemonDBManager.close()
     LemonDBManager.removeTask("TSKQKG042422")
     LemonFileManager.deleteTaskDirectory("TSKQKG042422")
 
